0d1c79275191aaa751ce1d1fe6cf0726137de935
carlos <carlosalaniz>
2017-03-22 23:47:38 -0500

    some

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 $myfile = fopen("webcar.log", "w") or die("Unable to open file!");
 foreach($_POST as $k => $v){
+    echo "POST_".json_encode([$k=>$v]).'\n';
     fwrite($myfile, "POST_".json_encode([$k=>$v]));
 }
 
 foreach($_GET as $k => $v){
+    echo "GET_".json_encode([$k=>$v]).'\n';
     fwrite($myfile, "GET_".json_encode([$k=>$v]));
 }
 fclose($myfile);
