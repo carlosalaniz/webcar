72fe973cb4531c8a9e46484d8d1e4ebbebd7792c
carlos <carlosalaniz>
2017-03-23 00:01:35 -0500

    some

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $file = "webcar.log";
 foreach($_POST as $k => $v){
-    $message = "POST_".json_encode([$k=>$v]).'\n';
+    $message = "POST_".json_encode([$k=>$v]).'/n';
     file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
 }
 foreach($_GET as $k => $v){
-    $message = "GET_".json_encode([$k=>$v]).'\n';
+    $message = "GET_".json_encode([$k=>$v]).'/n';
     file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
 }
 echo "OK!";
