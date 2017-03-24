54ab8539e1e9c55dca3cae4070c98da3d7735cbf
carlos <carlosalaniz>
2017-03-24 04:05:35 -0500

    s

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ file_put_contents($file, $message.',', FILE_APPEND | LOCK_EX);
 
 $require("./phpmqtt.php");
 	
-$mqtt = new phpMQTT("broker.hivemq.com", 
-1883, "lksdhfjhsdj-4"); //Change client name to something unique
+$mqtt = new phpMQTT("broker.hivemq.com", 1883, "lksdhfjhsdj-4"); //Change client name to something unique
 if ($mqtt->connect()) {
 	$mqtt->publish("92361f002671/mazda01",$message,0);
 	$mqtt->close();
