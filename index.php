b1d0c862992b1bb20302d80c41284ee38c0561a2
carlos <carlosalaniz>
2017-03-24 03:36:57 -0500

    d

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 $file = "webcar4.log";
 $message = json_encode($_GET);
-file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
+file_put_contents($file, $message.',', FILE_APPEND | LOCK_EX);
 echo "OK!";
