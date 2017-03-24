846ee449bbc2c5265d1d45d4ea8e91040aa843bb
carlos <carlosalaniz>
2017-03-24 03:49:17 -0500

    s

--- testMosquitto.php
+++ testMosquitto.php
@@ -1,29 +1,14 @@
 <?php
-$client = new Mosquitto\Client();
-$client->onConnect('connect');
-$client->onDisconnect('disconnect');
-$client->onSubscribe('subscribe');
-$client->onMessage('message');
-$client->connect("broker.hivemq.com", 8000, 5);
-$client->subscribe('/#', 1);
-while (true) {
-	$client->loop();
-	$mid = $client->publish('92361f002671/mazda01', "Hello from PHP at " . date('Y-m-d H:i:s'), 1, 0);
-	echo "Sent message ID: {$mid}\n";
-	$client->loop();
-	sleep(2);
+
+$c = new Mosquitto\Client;
+$c->onConnect(function() use ($c) {
+    $c->publish('92361f002671/mazda01', 'Hello', 2);
+});
+
+$client->connect("broker.hivemq.com", 8000);
+for ($i = 0; $i < 100; $i++) {
+    // Loop around to permit the library to do its work
+    $c->loop(1);
 }
-$client->disconnect();
-unset($client);
-function connect($r) {
-	echo "I got code {$r}\n";
-}
-function subscribe() {
-	echo "Subscribed to a topic\n";
-}
-function message($message) {
-	printf("Got a message ID %d on topic %s with payload:\n%s\n\n", $message->mid, $message->topic, $message->payload);
-}
-function disconnect() {
-	echo "Disconnected cleanly\n";
-}
+
+echo "Finished\n";
