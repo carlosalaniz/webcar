e5390daa6d094de8767f61202a84b79e7d8079bc
carlos <carlosalaniz>
2017-03-24 03:32:17 -0500

    s

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $file = "webcar2.log";
-if(!file_exists('http://carlosalaniz.me/webcar/webcar2.log'.$file)){
+if(!file_exists('./'.$file)){
     file_put_contents($file, '[' ,  FILE_APPEND | LOCK_EX);
 }
 
