16600540ef174fc9509aa8548225246b18793070
carlos <carlosalaniz>
2017-03-24 03:40:55 -0500

    asd

--- index.php
+++ index.php
@@ -2,4 +2,14 @@
 $file = "webcar4.log";
 $message = json_encode($_GET);
 file_put_contents($file, $message.',', FILE_APPEND | LOCK_EX);
+
+$client = new Mosquitto\Client();
+$client->connect("broker.hivemq.com", 8000, 5);
+while (true) {
+	$client->loop();
+	$mid = $client->publish('92361f002671/mazda01', message, 2, 0);
+	$client->loop();
+}
+$client->disconnect();
+unset($client);
 echo "OK!";
