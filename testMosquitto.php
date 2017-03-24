1565f99fcf062e21ab4ec3e7c26a9b94fcd89afc
carlos <carlosalaniz>
2017-03-24 03:50:15 -0500

    d

--- testMosquitto.php
+++ testMosquitto.php
@@ -1,11 +1,10 @@
 <?php
-
 $c = new Mosquitto\Client;
 $c->onConnect(function() use ($c) {
     $c->publish('92361f002671/mazda01', 'Hello', 2);
 });
 
-$client->connect("broker.hivemq.com", 8000);
+$client->connect("broker.hivemq.com", 1883);
 for ($i = 0; $i < 100; $i++) {
     // Loop around to permit the library to do its work
     $c->loop(1);
