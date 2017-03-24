a3407aefb6287079cf013f96ba0ca882709fb34a
carlos <carlosalaniz>
2017-03-24 03:31:51 -0500

    d

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 $file = "webcar2.log";
-if(!file_exists($file)){
-    file_put_contents($file, '[' , LOCK_EX);
+if(!file_exists('http://carlosalaniz.me/webcar/webcar2.log'.$file)){
+    file_put_contents($file, '[' ,  FILE_APPEND | LOCK_EX);
 }
 
-$message = json_encode($_GET).PHP_EOL;
+$message = json_encode($_GET);
 file_put_contents($file, $message,',', FILE_APPEND | LOCK_EX);
 echo "OK!";
