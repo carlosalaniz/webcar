50709a3b15403f45d94fef8b5e0ff04e4f01d0c0
carlos <carlosalaniz>
2017-03-24 03:32:51 -0500

    sdf

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
 $file = "webcar2.log";
-if(!file_exists('./'.$file)){
-    file_put_contents($file, '[' ,  FILE_APPEND | LOCK_EX);
-}
 
 $message = json_encode($_GET);
 file_put_contents($file, $message,',', FILE_APPEND | LOCK_EX);
