b27477b63372deee1e46b44f79ef4af4cbba9a3e
carlos <carlosalaniz>
2017-03-24 03:29:56 -0500

    ss

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+
 $file = "webcar2.log";
-file_put_contents($file, PHP_EOL.PHP_EOL.PHP_EOL, FILE_APPEND | LOCK_EX);
+if(!file_exists($file)){
+    file_put_contents($file, '[' , LOCK_EX);
+}
 
-    $message = json_encode($_GET).PHP_EOL;
-    file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
-
+$message = json_encode($_GET).PHP_EOL;
+file_put_contents($file, $message,',', FILE_APPEND | LOCK_EX);
 echo "OK!";
