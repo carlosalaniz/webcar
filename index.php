b442f661f5af2e78739d935cff0832e56628504d
carlos <carlosalaniz>
2017-03-24 04:17:31 -0500

    session

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(-1);
 ini_set('error_reporting', E_ALL);
 require("./phpmqtt.php");
-$file = "webcar5.log";
+$file = "NewSession.log";
 $message = json_encode($_GET);
 file_put_contents($file, $message.',', FILE_APPEND | LOCK_EX);	
 $mqtt = new phpMQTT("broker.hivemq.com", 1883, "lksdhfjhsdj-4"); //Change client name to something unique
--- test.js
+++ test.js
@@ -4,19 +4,17 @@ app.controller('myCtrl', ['$scope', 'MqttClient', function($scope, MqttClient) {
  var ip = "broker.hivemq.com";
     var port = "8000";
     var id = "clientId-yzUBL20ktx";
-    $scope.lastMessage = "empty";
+    $scope.lastMessage = {};
     MqttClient.init(ip, port, id);
     MqttClient.connect({onSuccess: successCallback});
     function successCallback() {
       MqttClient.subscribe('92361f002671/mazda01');
-      message = new Paho.MQTT.Message("Hello");
-      message.destinationName = "92361f002671/mazda01";
-      MqttClient.send(message);
     }
 
     $scope.$on('message-arrived', function(event, args) {
         var message = args.lastMessage;
-        $scope.lastMessage = message.payloadString;
+        console.log(message.payloadString);
+        $scope.lastMessage = JSON.parse(message.payloadString);
         $scope.$apply();
     });
 
