32faa4c049327c088f1c7cfb8b01cfddb15d141f
carlos <carlosalaniz>
2017-03-24 03:54:09 -0500

    asdasdasd

--- testMosquitto.php
+++ testMosquitto.php
@@ -1,13 +1,9 @@
 <?php
-$c = new Mosquitto\Client;
-$c->onConnect(function() use ($c) {
-    $c->publish('92361f002671/mazda01', 'Hello', 2);
-});
-
-$client->connect("broker.hivemq.com", 1883);
-for ($i = 0; $i < 100; $i++) {
-    // Loop around to permit the library to do its work
-    $c->loop(1);
+require("./phpmqtt.php");
+	
+$mqtt = new phpMQTT("broker.hivemq.com", 
+1883, "lksdhfjhsdj-4"); //Change client name to something unique
+if ($mqtt->connect()) {
+	$mqtt->publish("92361f002671/mazda01","Hello World! at ".date("r"),0);
+	$mqtt->close();
 }
-
-echo "Finished\n";
