7765e30dafd9c4a2d491f3baa3264285887e6124
carlos <carlosalaniz>
2017-03-24 03:26:15 -0500

    ss

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-$file = "webcar.log";
+$file = "webcar2.log";
 file_put_contents($file, PHP_EOL.PHP_EOL.PHP_EOL, FILE_APPEND | LOCK_EX);
-foreach($_POST as $k => $v){
-    $message = json_encode([$k=>$v]).PHP_EOL;
-    file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
-}
 foreach($_GET as $k => $v){
-    $message = json_encode([$k=>$v]).PHP_EOL;
+    $message = json_encode($_GET).PHP_EOL;
     file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
 }
 echo "OK!";
