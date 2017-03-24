7c40efe67d09cd88010daf05d81476f929b0cea7
carlos <carlosalaniz>
2017-03-24 04:51:49 -0500

    new folder structure

--- testMosquitto.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("./phpmqtt.php");
-	
-$mqtt = new phpMQTT("broker.hivemq.com", 
-1883, "lksdhfjhsdj-4"); //Change client name to something unique
-if ($mqtt->connect()) {
-	$mqtt->publish("92361f002671/mazda01","Hello World! at ".date("r"),0);
-	$mqtt->close();
-}
